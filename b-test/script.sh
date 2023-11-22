203cd9200c050290d0031391c474f1f8fa4961bc
ulyh <janeuly@pm.me>
2023-11-22 11:59:56 +0100

    modified script

--- b-test/script.sh
+++ b-test/script.sh
@@ -4,3 +4,4 @@ echo "Running a client-ci container..."
 docker pull ulyhmyges/client-ci
 docker run --name client-ci-container -d ulyhmyges/client-ci
 docker logs -f client-ci-container
+echo "End of script"
